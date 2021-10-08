b12c395fa003814716ca468f8c15ce6c31d0ae14
Tornad00 <91788538+Tornad00@users.noreply.github.com>
2021-10-08 16:17:46 +0000

    lec_1_done

---
+++ lec_1_input.py
@@ -0,0 +1,10 @@
+a = input ()
+print (a)
+
+a = input("  значение а: ")
+print(a)
+print( type (a))
+
+a = int(input( "введите число" ))
+print(a)
+print ( type (a))
---
+++ lec_1_str.py
@@ -0,0 +1,15 @@
+a = "Good"
+b = "Bad"
+print( a + b)
+
+#print( a - b) так нельзя
+
+print(a * 3)
+
+c = 50
+
+d = 10 
+
+print (f"вставим числа {c} и {d}")
+
+print (len(a))
