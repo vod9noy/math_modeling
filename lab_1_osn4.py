ad7437bca137e24eb641d50b16900f66e29be13e
Tornad00 <91788538+Tornad00@users.noreply.github.com>
2021-10-12 18:49:57 +0000

    сделаль

--- lab_1_osn4.py
+++ lab_1_osn4.py
@@ -1,4 +1,13 @@
-a = [1, 5, ‘Good’, ‘Bad’]
+a = [1, 5, 'Good', 'Bad']
 
-b = [9, ‘Blue’, ‘Red’, 11]
+b = [9, 'Blue', 'Red', 11]
 
+print ( a[1] + b[3])
+
+print ( a[2] + b[2])
+
+print ( a[0] + b[0])
+
+print ( (a[1] ** 2) + b[3])
+
+print ( (a) + (b))
